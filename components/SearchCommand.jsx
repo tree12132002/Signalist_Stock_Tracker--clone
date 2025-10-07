d0accc6857d3c42d5ac357432d0fe1d784ce01d6
Raven Ling <tree12132002@hotmail.com>
2025-10-08 00:16:31 +0800

    make the rabbit happy

--- components/SearchCommand.jsx
+++ components/SearchCommand.jsx
@@ -8,7 +8,7 @@ import {
   CommandList,
 } from '@/components/ui/command'
 import { Button } from '@/components/ui/button'
-import { Loader2, Star, TrendingUp } from 'lucide-react'
+import { Loader2, TrendingUp } from 'lucide-react'
 import Link from 'next/link'
 import { searchStocks } from '@/lib/actions/finnhub-actions'
 import { useDebounce } from '@/hooks/useDebounce'
